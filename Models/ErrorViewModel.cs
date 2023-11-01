f22bb33124968d7208efe3c1749a5375f0c09628
JanSterle1337 <jan.sterle123@gmail.com>
2023-11-01 16:44:52 +0100

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace healthcare_system.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
