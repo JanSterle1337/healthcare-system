b0c7769130f60b11c4bf7192b2799d21d55f4c6f
jan.sterle123@gmail.com <jan.sterle123@gmail.com>
2023-11-07 17:50:08 +0100

    Modification of db context and db connection string to postgres db

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -5,9 +5,16 @@ namespace healthcare_system.Data
 {
     public class ApplicationDbContext: DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        protected readonly IConfiguration Configuration;
+
+        public ApplicationDbContext(IConfiguration configuration)
         {
-            
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseNpgsql(Configuration.GetConnectionString("WebApiDatabase"));
         }
 
         public DbSet<Patient> Patients { get; set; }
---
+++ Migrations/20231107163436_init.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace healthcare_system.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Patients",
+                columns: table => new
+                {
+                    Uuid = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: false),
+                    Birth = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Sex = table.Column<string>(type: "character varying(1)", maxLength: 1, nullable: false),
+                    Age = table.Column<int>(type: "integer", nullable: false),
+                    EmailAddress = table.Column<string>(type: "text", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patients", x => x.Uuid);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Patients");
+        }
+    }
+}
