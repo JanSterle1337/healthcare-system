1517e0ea729df082a0105c2c678033aaa01a1ead
jan.sterle123@gmail.com <jan.sterle123@gmail.com>
2023-11-07 15:15:26 +0100

    ApplicationDbContext, Patient Model and entityframework dependencies

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace healthcare_system.Data
+﻿using healthcare_system.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace healthcare_system.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext: DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Patient> Patients { get; set; }
     }
 }
